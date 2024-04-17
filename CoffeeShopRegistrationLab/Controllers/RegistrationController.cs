68ff6750f8f7274c18a1cf59e85f1cd02d23d3db
kmundie2 <mundiek@charter.net>
2024-04-16 22:17:09 -0400

    Add project files.

---
+++ CoffeeShopRegistrationLab/Controllers/RegistrationController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+public class RegistrationController : Controller
+{
+    public ActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public ActionResult Index(UserRegistrationModel model)
+    {
+
+        ViewBag.WelcomeMessage = $"Welcome, {model.FirstName}!";
+        return View("Result");
+    }
+}
---
+++ CoffeeShopRegistrationLab/Models/UserRegistrationModel.cs
@@ -0,0 +1,8 @@
+﻿public class UserRegistrationModel
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+}
+
